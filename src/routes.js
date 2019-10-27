ea6562d0e31ea2f064f9b4a811471e873efe3e6b
Wesley Hott Soares <wesley.hott@gmail.com>
2019-10-27 20:48:50 -0300

    feat: muler

--- src/routes.js
+++ src/routes.js
@@ -1,10 +1,15 @@
 import { Router } from 'express';
+import multer from 'multer';
+import multerConfig from './config/multer';
+
+
 // import User from './app/models/User';
 import UserController from './app/controllers/UserController';
 import SessionController from './app/controllers/SessionController';
 
 import authMiddleware from './app/middlewares/auth'
 const routes = new Router();
+const upload = multer(multerConfig);
 
 // routes.get('/', (req, res) => {
 //   return res.json({ message: 'hello world' });
@@ -22,5 +27,8 @@ routes.post('/sessions', SessionController.store);
 
 routes.use(authMiddleware); // somente as rotas que vem depois usam esse middleware
 routes.put('/users', UserController.update);
+routes.post('/files', upload.single('file'), (req, res) => {
+    return res.json({ ok: true });
+})
 
 export default routes;
