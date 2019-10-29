6508a4e2e6fa4a73f6a772428ad202a90d1e2a0e
Wesley Hott Soares <wesley.hott@gmail.com>
2019-10-29 15:23:16 -0300

    feat: exibição de agendamentos

--- src/app/middlewares/auth.js
+++ src/app/middlewares/auth.js
@@ -20,6 +20,7 @@ export default async (req, res, next) => {
   try {
     const decoded = await promisify(jwt.verify)(token, authConfig.secret);
     req.userId = decoded.id; // decoded tem as três partes decodificadas
+    return next();
   } catch (err) {
     res.status(401).json({ error: 'Token invalid' });
   }
