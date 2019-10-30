527c54d879a6d174ccae84a05a72872890039925
Wesley Hott Soares <wesley.hott@gmail.com>
2019-10-30 19:21:10 -0300

    feat: email

---
+++ src/config/mail.js
@@ -0,0 +1,12 @@
+export default {
+  host: 'smtp.mailtrap.io',
+  port: 2525,
+  auth: {
+    user: '7be44a54ebaa28',
+    pass: '4dcecf617bad4c',
+  },
+  secure: false,
+  default: {
+    from: 'Equipe GoBarber <noreply@gobarber.com>',
+  },
+};
