427b7d128c895fbf1f0c9967327ad0b7854c23e9
Wesley Hott Soares <wesley.hott@gmail.com>
2019-10-30 21:09:57 -0300

    feat: engine mail

--- src/app/controllers/AppointmentController.js
+++ src/app/controllers/AppointmentController.js
@@ -120,6 +120,11 @@ class AppointmentController {
           as: 'provider',
           attributes: ['name', 'email'],
         },
+        {
+          model: User,
+          as: 'user',
+          attributes: ['name'],
+        },
       ],
     });
 
@@ -143,7 +148,14 @@ class AppointmentController {
     await Mail.sendMail({
       to: `${appointment.provider.name} <${appointment.provider.email}>`,
       subject: 'Agendamento cancelado',
-      text: 'Você tem um novo cancelamento',
+      template: 'cancelation',
+      context: {
+        provider: appointment.provider.name,
+        user: appointment.user.name,
+        date: format(appointment.date, "'dia' dd 'de' MMMM', às 'H'h'mm", {
+          locale: ptBR,
+        }),
+      },
     });
 
     return res.json(appointment);
